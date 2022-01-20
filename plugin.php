c4ac7943d92dcce12be9cf43a46da479a7624ada
Chris Reynolds <chris@jazzsequence.com>
2022-01-20 14:31:52 -0700

    add a base plugin file

---
+++ plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Pantheon Edge Integrations
+ * Description: WordPress plugin to support Pantheon Edge Integrations and personalization features.
+ * Author: Pantheon
+ * Author URI: https://pantheon.io
+ * Version: 0.1
+ *
+ * @package Pantheon/EdgeIntegrations
+ */
+
+namespace Pantheon\EI;
+
+// Let's goooo!
+bootstrap();
