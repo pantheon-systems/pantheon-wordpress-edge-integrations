9a41454fec405685affe5c9fdc7ebc08e2b8dcfc
Chris Reynolds <chris@jazzsequence.com>
2022-01-20 14:32:04 -0700

    add a base namespace file

---
+++ inc/namespace.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Pantheon Edge Integrations main namespace file.
+ *
+ * Handles global edge integrations functionality.
+ *
+ * @package Pantheon/EdgeIntegrations
+ */
+
+namespace Pantheon\EI;
+
+/**
+ * Kick it off!
+ */
+function bootstrap() {
+	// Action hooks and filters go here.
+}
