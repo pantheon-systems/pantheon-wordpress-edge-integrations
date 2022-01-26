ee06eda53494bb99f4bfb90fcae0cab089afb013
Chris Reynolds <chris@jazzsequence.com>
2022-01-26 15:09:57 -0700

    add a very simple proof of life test

---
+++ tests/Test.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Main integration tests for WordPress Edge Integrations.
+ *
+ * @package Pantheon\EdgeIntegrations
+ */
+
+use Pantheon\EI;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Main test class for WordPress Edge Integrations plugin.
+ */
+class testsBase extends TestCase {
+
+	/**
+	 * Make sure unit tests are running.
+	 */
+	public function testPHPUnitIsWorking() {
+		$this->assertTrue( true );
+	}
+}
