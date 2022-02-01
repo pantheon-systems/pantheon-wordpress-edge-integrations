a8752c2ab6c6ce7bc7efc7f14f9f5673fe19ee7b
jspellman814 <john.spellman@pantheon.io>
2022-02-01 13:59:13 -0700

    Add build process for JS files

---
+++ config/webpack.settings.js
@@ -0,0 +1,40 @@
+// Webpack settings exports.
+module.exports = {
+	entries: {
+		// JS files.
+		assets: './assets/js/assets.js',
+	},
+	filename: {
+		js: 'js/[name].js',
+	},
+	paths: {
+		src: {
+			base: './assets/',
+			js: './assets/js/',
+		},
+		dist: {
+			base: './dist/',
+			clean: ['./js'],
+		},
+	},
+	stats: {
+		// Copied from `'minimal'`.
+		all: false,
+		errors: true,
+		maxModules: 0,
+		modules: true,
+		warnings: true,
+		// Our additional options.
+		assets: true,
+		errorDetails: true,
+		excludeAssets: /\.(jpe?g|png|gif|svg|woff|woff2)$/i,
+		moduleTrace: true,
+		performance: true,
+	},
+	performance: {
+		maxAssetSize: 100000,
+	},
+	manifestConfig: {
+		basePath: '',
+	},
+};
