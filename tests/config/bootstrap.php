5f2112456ce99a44c81cc36d089b1f8f505b3ae7
Chris Reynolds <chris@jazzsequence.com>
2022-02-04 16:23:20 -0700

    bootstrap some WordPress stuff

--- tests/config/bootstrap.php
+++ tests/config/bootstrap.php
@@ -5,7 +5,25 @@
  * @package Pantheon/EdgeIntegrations
  */
 
+use Pantheon\EI\WP;
+
 /**
  * Call the autoloader
  */
 require_once __DIR__ . '/../../vendor/autoload.php';
+
+/**
+ * Bootstrap WordPress.
+ */
+defined( 'ABSPATH' ) or define( 'ABSPATH', __DIR__ . '/../../vendor/wordpress/wordpress/src/' );
+defined( 'WPINC' ) or define( 'WPINC', 'wp-includes' );
+require_once ABSPATH . WPINC . '/default-constants.php';
+require_once ABSPATH . WPINC . '/functions.php';
+require_once ABSPATH . WPINC . '/load.php';
+require_once ABSPATH . WPINC . '/plugin.php';
+wp_initial_constants();
+
+/**
+ * Bootsrap the plugin
+ */
+WP\bootstrap();
