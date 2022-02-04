7885bf10fb3f9a9c55a8ccc1f06e331f81a3ba15
Chris Reynolds <chris@jazzsequence.com>
2022-02-04 16:24:12 -0700

    make sure the geo namespace was loaded

---
+++ tests/geoTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Test Suite for Edge Integrations Geo.
+ *
+ * @package Pantheon\EdgeIntegrations
+ */
+
+use Pantheon\EI;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Main test class for WordPress Edge Integrations plugin.
+ */
+class geoTests extends TestCase {
+
+	/**
+	 * Make sure unit tests are running.
+	 */
+	public function testBootstrapSloaded() {
+		$this->assertTrue( function_exists( '\\Pantheon\\EI\\WP\\Geo\\bootstrap' ) );
+	}
+}
