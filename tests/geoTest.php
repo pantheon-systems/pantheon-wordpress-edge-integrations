a96ad2065754b4d1d47acd4f8d6436dff650ea74
Chris Reynolds <chris@jazzsequence.com>
2022-02-08 10:14:15 -0700

    add a failing test message

--- tests/geoTest.php
+++ tests/geoTest.php
@@ -18,6 +18,9 @@ class geoTests extends TestCase {
 	 * Make sure unit tests are running.
 	 */
 	public function testBootstrapIsLoaded() {
-		$this->assertTrue( function_exists( '\\Pantheon\\EI\\WP\\Geo\\bootstrap' ) );
+		$this->assertTrue(
+			function_exists( '\\Pantheon\\EI\\WP\\Geo\\bootstrap' ),
+			'bootstrap function does not exist'
+		);
 	}
 }
