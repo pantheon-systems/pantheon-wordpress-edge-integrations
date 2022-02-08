2909309bef58682cb62efd18acef819b5d1655dd
Chris Reynolds <chris@jazzsequence.com>
2022-02-08 10:14:44 -0700

    add a get_geo function

--- inc/geo.php
+++ inc/geo.php
@@ -18,3 +18,27 @@ function bootstrap() {
 		return __NAMESPACE__ . "\\$callback";
 	};
 }
+
+/**
+ * Return geolocation data for the current user.
+ *
+ * @param string $data_type The type of geo data to return. Allowed values: 'country', 'region', 'city', 'postal-code', 'latlon'. All other values will return an empty string. Defaults to 'country'.
+ *
+ * @param mixed $data Data to pass to the HeaderData class. By default, this is pulled from $_SERVER data.
+ *
+ * @return string The requested geo data.
+ */
+function get_geo( string $data_type = 'country', $data = null ) : string {
+	$allowed_values = [ 'country', 'region', 'city', 'postal-code', 'latlon' ];
+
+	if ( ! in_array( $data_type, $allowed_values, true ) ) {
+		return '';
+	}
+
+	// The default 'geo' parameter returns the country code. Let 'country' pull the 'geo' data.
+	$data_type = $data_type === 'country' ? 'geo' : $data_type;
+	$parsed_geo = EI\HeaderData::parse( 'Audience', $data );
+	$geo = $parsed_geo[ $data_type ];
+
+	return $geo;
+}
