e0f2ac91dcf0b4d10f9fcd3ac4d1d8a175064494
Chris Reynolds <chris@jazzsequence.com>
2022-02-08 12:06:26 -0700

    allow an empty string to return all geo data

--- inc/geo.php
+++ inc/geo.php
@@ -22,20 +22,27 @@ function bootstrap() {
 /**
  * Return geolocation data for the current user.
  *
- * @param string $data_type The type of geo data to return. Allowed values: 'country', 'region', 'city', 'postal-code', 'lat', 'lon', 'latlon'. All other values will return an empty string. Defaults to 'country'. 'geo' is allowed as an alias for 'country', but the latter is recommended.
+ * @param string $data_type The type of geo data to return. Allowed values: 'country', 'region', 'city', 'postal-code', 'lat', 'lon', 'latlon' or an empty string. All other values will return an empty string. Defaults to ''. 'geo' is allowed as an alias for 'country', but the latter is recommended.
+ *
+ * If an empty string is passed, get_geo() will return all Audience data encoded in JSON format.
  *
  * @param mixed $data Data to pass to the HeaderData class. By default, this is pulled from $_SERVER data.
  *
  * @return string The requested geo data.
  */
-function get_geo( string $data_type = 'country', $data = null ) : string {
-	$allowed_values = [ 'geo', 'country', 'region', 'city', 'postal-code', 'lat', 'lon', 'latlon' ];
+function get_geo( string $data_type = '', $data = null ) : string {
+	$allowed_values = [ '', 'geo', 'country', 'region', 'city', 'postal-code', 'lat', 'lon', 'latlon' ];
 
 	if ( ! in_array( $data_type, $allowed_values, true ) ) {
 		return '';
 	}
 
 	$parsed_geo = EI\HeaderData::parse( 'Audience', $data );
+
+	// If no geo data type was passed, return all Audience data.
+	if ( empty( $data_type ) ) {
+		return json_encode( $parsed_geo );
+	}
 
 	// The default 'geo' parameter returns the country code. Let 'country' pull the 'geo' data.
 	$data_type = $data_type === 'country' ? 'geo' : $data_type;
