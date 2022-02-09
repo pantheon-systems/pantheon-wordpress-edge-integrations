30ea3938f63092b654abcef67a2ab68a30a6ee76
jspellman814 <john.spellman@pantheon.io>
2022-02-09 09:29:12 -0700

    More test driven development

--- tests/interestTest.php
+++ tests/interestTest.php
@@ -6,6 +6,7 @@
  */
 
 use Pantheon\EI;
+use Pantheon\EI\WP\Interest;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -24,22 +25,80 @@ class interestsTests extends TestCase {
 	}
 
 	/**
-	 * Test script registration.
+	 * Test Interest post types.
 	 */
 	public function testRegisterScript() {
 		$this->assertTrue( 
-			function_exists( '\\Pantheon\\EI\\WP\\Interest\\register_script' ),
-			'register_script function does not exist'
+			function_exists( '\\Pantheon\\EI\\WP\\Interest\\get_interest_allowed_post_types' ),
+			'get_interest_allowed_post_types function does not exist'
 		);
 	}
 
 	/**
-	 * Test Interest localization.
+	 * Test Interest taxonomy.
 	 */
-	public function testInterestLocalization() {
+	public function testInterestTaxomony() {
 		$this->assertTrue( 
-			function_exists( '\\Pantheon\\EI\\WP\\Interest\\localize_interests' ),
-			'localize_interests function does not exist'
+			function_exists( '\\Pantheon\\EI\\WP\\Interest\\get_interest_taxonomy' ),
+			'get_interest_taxonomy function does not exist'
+		);
+	}
+
+	/**
+	 * Test Interest threshold.
+	 */
+	public function testInterestThreshold() {
+		$this->assertTrue( 
+			function_exists( '\\Pantheon\\EI\\WP\\Interest\\get_interest_threshold' ),
+			'get_interest_threshold function does not exist'
+		);
+	}
+
+	/**
+	 * Test the pantheon.ei.post_types filter.
+	 */
+	public function testInterestPostTypesFilter() {
+		// Filter the interest threshold.
+		add_filter( 'pantheon.ei.post_types', function( $value ) {
+			return [ 'post', 'a-custom-post-type' ];
+		}, 10, 1 );
+
+		$this->assertEquals(
+			Interest\get_interest_allowed_post_types(),
+			[ 'post', 'a-custom-post-type' ],
+			'Filtered post types do not match'
+		);
+	}
+
+	/**
+	 * Test the pantheon.ei.taxonomy filter.
+	 */
+	public function testInterestTaxonomyFilter() {
+		// Filter the interest taxonomy.
+		add_filter( 'pantheon.ei.taxonomy', function( $value ) {
+			return [ 'category', 'test-category' ];
+		}, 10, 1 );
+
+		$this->assertEquals(
+			Interest\get_interest_taxonomy(),
+			[ 'category', 'test-category' ],
+			'Filtered taxonomy does not match'
+		);
+	}
+
+	/**
+	 * Test the pantheon.ei.interest_threshold filter.
+	 */
+	public function testInterestThresholdFilter() {
+		// Filter the interest threshold.
+		add_filter( 'pantheon.ei.interest_threshold', function( $value ) {
+			return 5;
+		}, 10, 1 );
+
+		$this->assertEquals(
+			Interest\get_interest_threshold(),
+			5,
+			'Filtered threshold does not match'
 		);
 	}
 }
