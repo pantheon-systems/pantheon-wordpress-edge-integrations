1f5b15f76f00f5d4f7098df0bbd591f1326684cb
Chris Reynolds <chris@jazzsequence.com>
2022-03-28 12:55:54 -0600

    re-add webpack config

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,10 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.config.common.js');
+
+// Config files.
+const settings = require('./webpack.settings.js');
+
+module.exports = merge(common, {
+	mode: 'development',
+	devtool: 'inline-cheap-module-source-map',
+});
