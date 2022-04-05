cb5d51d7e374e05a8e328a8f2e5e04fd20fee346
Chris Reynolds <chris@jazzsequence.com>
2022-04-05 09:32:40 -0600

    bump versions

--- pantheon-wordpress-edge-integrations.php
+++ pantheon-wordpress-edge-integrations.php
@@ -4,7 +4,7 @@
  * Description: WordPress plugin to support Pantheon Edge Integrations and personalization features.
  * Author: Pantheon
  * Author URI: https://pantheon.io
- * Version: 0.2.14-dev
+ * Version: 0.2.15-dev
  *
  * @package Pantheon/EdgeIntegrations
  */
