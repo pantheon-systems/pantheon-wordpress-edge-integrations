d34e0d614654e56012c92d6dbe2ba84add463d6b
Chris Reynolds <chris@jazzsequence.com>
2022-04-06 14:27:58 -0600

    add an admin.php namespace to handle admin stuff

---
+++ inc/admin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Pantheon Edge Integrations admin interface.
+ *
+ * Handles admin features and settings for the Pantheon Edge Integrations plugin.
+ *
+ * @package Pantheon/EdgeIntegrations
+ */
+
+namespace Pantheon\EI\WP\Admin;
+
+/**
+ * Kick off the back-end.
+ */
+function bootstrap() {}
