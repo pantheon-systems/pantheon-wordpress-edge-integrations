6850c5181f0d55b4c8bcbee754aaa25c92ec77e2
Chris Reynolds <chris@jazzsequence.com>
2022-06-24 11:27:00 -0600

    bootstrap and add an endpoint

--- inc/api.php
+++ inc/api.php
@@ -13,3 +13,19 @@ use WP_REST_Server;
 
 const API_NAMESPACE = 'pantheon/v1/ei';
 
+/**
+ * Kickoff the WP REST API integration.
+ */
+function bootstrap() {
+	add_action( 'rest_api_init', __NAMESPACE__ . '\\register_endpoints' );
+}
+
+/**
+ * Register API routes.
+ */
+function register_endpoints() {
+	register_rest_route( API_NAMESPACE, 'segments', [
+		'method' => WP_REST_Server::READABLE,
+		'callback' => __NAMESPACE__ . '\\get_available_segments',
+	] );
+}
