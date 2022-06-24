3dc90c71bece374c8630b1f33d549958b68cedcb
Chris Reynolds <chris@jazzsequence.com>
2022-06-24 10:20:43 -0600

    add an api namespace

---
+++ inc/api.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Pantheon Edge Integrations WP REST API integration.
+ *
+ * Provides API endpoints for Edge Integrations.
+ *
+ * @package Pantheon/EdgeIntegrations
+ */
+
+namespace Pantheon\EI\WP\API;
+
+function bootstrap() {}
